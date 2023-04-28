1972eb553589f92265f34b9becdbb30b45ffb545
TinaPera <valentina.pera2007@gmail.com>
2023-04-28 15:00:19 -0300

    projeto2-teste

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const cidades = ref (['São Paulo',
+    'Rio de Janeiro',
+    'Belo Horizonte',
+    'Salvador',
+    'Fortaleza',
+    'Curitiba',
+    'Manaus',
+    'Recife',
+    'Porto Alegre',
+    'Brasília'])
+
+
+</script>
+
+<template>
+<ul>
+  <li> 
+   
+  </li>
+</ul>
+</template>
+
+<style scoped>
+
+</style>
