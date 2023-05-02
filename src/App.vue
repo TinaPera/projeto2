24e7d133fb4e978506a97bc12653bdf13fac8728
TinaPera <valentina.pera2007@gmail.com>
2023-05-02 11:42:41 -0300

    abule

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <script setup>
-import { ref } from 'vue'
-
-const cidades = ref (['São Paulo',
-    'Rio de Janeiro',
-    'Belo Horizonte',
-    'Salvador',
-    'Fortaleza',
-    'Curitiba',
-    'Manaus',
-    'Recife',
-    'Porto Alegre',
-    'Brasília'])
-
+const produtos = [
+    {
+        id: 1,
+        nome: 'Camiseta',
+        preco: 49.90
+    },
+    {
+        id: 2,
+        nome: 'Calça',
+        preco: 99.90
+    },
+    {
+        id: 3,
+        nome: 'Meia',
+        preco: 9.90
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        preco: 199.90
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        preco: 99.90
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        preco: 299.90
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        preco: 79.90
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        preco: 19.90
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90
+    }
+]
 
 </script>
 
-<template>
-<ul>
-  <li> 
-   
-  </li>
-</ul>
+<template><
+h1
 </template>
 
 <style scoped>
